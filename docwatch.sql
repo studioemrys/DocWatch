292449c999c1722ea12c8798296b8257b56b3a14
Studio Emrys <studio.emrys@gmail.com>
2017-09-13 21:47:00 +0200

    DocWatch alpha 0.1

---
+++ docwatch.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS `docwatch`;
+
+USE `docwatch`;
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+	`uuid` varchar(255) NOT NULL,
+	`role` char(2) DEFAULT NULL,
+	`avatar` varchar(255) DEFAULT NULL,
+	`initials` varchar(255) DEFAULT NULL,
+	`name` varchar(255) DEFAULT NULL,
+	`dob` date,
+	`sex` char(1) DEFAULT NULL,
+	`email` varchar(255) DEFAULT NULL,
+	`phone` char(10) DEFAULT NULL,
+	`city` varchar(255) DEFAULT NULL,
+	`phys` varchar(2047) DEFAULT NULL, 
+	`username` varchar(255) DEFAULT NULL,
+	`password` varchar(255) DEFAULT NULL,
+	`confirmed` tinyint(1) NOT NULL DEFAULT '0',
+	`wrong_logins` tinyint(1) NOT NULL DEFAULT '0',
+	UNIQUE (`uuid`),
+	UNIQUE (`avatar`),
+	UNIQUE (`username`),
+	PRIMARY KEY (`uuid`)
+);
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+DocWatchInstall()
+{
+	echo "Verify installation of MySQL..."
+	sleep 1
+	type mysql >/dev/null 2>&1 || { 
+		echo >&2 "MySQL is not installed..."
+		sleep 2
+		echo "Installation aborted"; 
+		exit 1; 
+	}
+	echo "MySQL installation verified"
+	sleep 1
+	echo "Create DocWatch database in MySQL.."
+	sleep 2
+	echo "Please enter your MySQL database password"
+	mysql -u root -p<docwatch.sql
+}
+clear
+echo ""
+echo "DOCWATCH INSTALLER"
+echo ""
+echo ""
+while true; do
+    read -p "Do you want to install DocWatch on your system? [Y/n]" jn
+    case $jn in
+        [Yy]* ) DocWatchInstall; break;;
+        [Nn]* ) exit;;
+        * )
+    esac
+done
+
